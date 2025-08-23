ec62e8208d848814a0a8366146b0bea6ebfaf69e
freevue <hackish113@gmail.com>
2025-08-23 11:02:31 +0900

    프로젝트 초기 설정: .gitignore, .prettierrc, jest.config.js, package.json, pnpm-lock.yaml, tsconfig.json 파일 추가

---
+++ jest.config.js
@@ -0,0 +1,14 @@
+export default {
+  verbose: true,
+  preset: 'ts-jest/presets/default-esm',
+  extensionsToTreatAsEsm: ['.ts'],
+  transform: {
+    '^.+\\.ts$': [
+      'ts-jest',
+      {
+        useESM: true,
+      },
+    ],
+  },
+  testEnvironment: 'node',
+}
