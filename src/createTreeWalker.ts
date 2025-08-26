4333287ca3509a7bfa48424429180f8491686abf
freevuehub <hackish113@gamil.com>
2025-08-26 21:06:18 +0900

    feat: createTreeWalker 함수에 브라우저 환경 체크 추가

--- src/createTreeWalker.ts
+++ src/createTreeWalker.ts
@@ -2,6 +2,8 @@
 // function createTreeWalker(show: number, node: Node): Generator<Node>
 
 function* createTreeWalker(show: number, node?: Node) {
+  if (document === undefined) throw new Error('브라우저 환경에서만 작동합니다.')
+
   if (node === undefined) return (currentNode: Node) => createTreeWalker(show, currentNode)
 
   const walker = document.createTreeWalker(node, show)
